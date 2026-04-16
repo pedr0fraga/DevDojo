d5958aaff7e0e929272b1f4395fc2224c506b75d
Pedro Fraga <pedrofraga128@gmail.com>
2026-04-16 12:59:41 -0300

    Segundo commit (até a aula 10)

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    /**
+     * Meu primeiro <b>Javadoc!</b>
+     */
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
+
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int numeroInteiro = 1;
+        double numeroDecimalMaisPreciso = 2.0;
+        float numeroDecimalMenosPreciso = 3.0f;
+        char caractere = 'A';
+        byte numeroMuitoPequeno = 127;
+        short numeroPequeno = 5000;
+        long numeroLongo = 20000000;
+        boolean verdadeiroOuFalso = false;
+
+
+        System.out.printf("Entre 'true' ou 'false', escolho " + verdadeiroOuFalso + ".");
+    }
+}
